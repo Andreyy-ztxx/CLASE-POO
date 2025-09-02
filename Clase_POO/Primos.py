5ed14518f1301fe3415f287f7f454ba46dbbb201
Andreyy-ztxx <andreysty2008@gmail.com>
2025-09-02 11:31:46 -0500

    Funcion para hallar numero primo

---
+++ Clase_POO/Primos.py
@@ -0,0 +1,13 @@
+#TAREA DE NUMEROS PRIMOS
+
+def Primos(numero):
+    if (numero < 2):
+        return False
+    for i in range (2, numero):
+        if (numero % i == 0):
+            return False
+        return True
+
+
+
+       
